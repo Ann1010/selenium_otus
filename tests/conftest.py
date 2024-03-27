adff75010a2a8e6268cd503155b93527461e8b9c
Анна Погребник <apogrebnik30@gmail.com>
2024-03-27 23:54:44 +0300

    hw12

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import pytest
 from selenium import webdriver
@@ -14,8 +15,13 @@ def pytest_addoption(parser):
     parser.addoption("--browser", default="chrome")
     parser.addoption("--url", "-U", default="http://192.168.0.133")
     parser.addoption("--headless", action="store_true")
+    parser.addoption("--bv",  default="91.0")
+    parser.addoption("--executor", action="store", default="127.0.0.1")
     parser.addoption("--drivers", default=os.path.expanduser("~/Downloads/drivers"))
-
+    parser.addoption("--vnc", action="store_true")
+    parser.addoption("--video", action="store_true")
+    parser.addoption("--logs", action="store_true")
+    
 
 @pytest.fixture()
 def url(request):
@@ -24,39 +30,71 @@ def url(request):
 @pytest.fixture()
 def browser(request, url):
     browser_name = request.config.getoption("--browser")
+    executor = request.config.getoption("--executor")
     headless = request.config.getoption("--headless")
     drivers = request.config.getoption("--drivers")
+    version = request.config.getoption("--bv")
+    vnc = request.config.getoption("--vnc")
+    video = request.config.getoption("--video")
+    logs = request.config.getoption("--logs")
+
+    executor_url = f"http://{executor}:4444/wd/hub"
 
     if browser_name == "chrome":
         service = ChromeService()
         options = ChromeOptions()
         if headless:
             options.add_argument("headless=new")
-        _browser = webdriver.Chrome(service=service)
+        # _browser = webdriver.Chrome(service=service)
     elif browser_name == "firefox":
         service = FirefoxService()
         options = FirefoxOptions()
         if headless:
             options.add_argument("headless")
-        _browser = webdriver.Firefox(service=service)
+        # _browser = webdriver.Firefox(service=service)
     elif browser_name == "yandex":
         options=webdriver.ChromeOptions()
         if headless:
             options.add_argument("headless=new")
         service = ChromeService(executable_path=os.path.join(drivers, "yandexdriver"))
         options.binary_location = "/usr/bin/yandex-browser"
-        _browser = webdriver.Chrome(service=service, options=options)
+        # _browser = webdriver.Chrome(service=service, options=options)
     elif browser_name == "safari":
         service = SafariService()
         options = SafariOptions()
         if headless:
             options.add_argument("headless=new")
-        _browser = webdriver.Safari(service=service)
+        #_browser = webdriver.Safari(service=service)
     else:
         raise Exception('Driver not supported')
+    
+    caps = {
+        "browserName": browser_name,
+        "browserVersion": version,
+        "selenoid:options": {
+            "enableVNC": vnc,
+            "name": os.getenv("BUILD_NUMBER", str(random.randint(9000, 10000))),
+            "screenResolution": "1280x2000",
+            "enableVideo": video,
+            "enableLog": logs,
+            "timeZone": "Europe/Moscow",
+            "env": ["LANG=ru_RU.UTF-8", "LANGUAGE=ru:en", "LC_ALL=ru_RU.UTF-8"]
 
-    _browser.set_window_size(3200, 5120)
-    _browser.get(url)
+        },
+        "acceptInsecureCerts": True,
+    }
+    
+    for k, v in caps.items():
+        options.set_capability(k, v)
+        print(k, v)
 
-    yield _browser
-    _browser.close()
+    driver = webdriver.Remote(
+        command_executor=executor_url,
+        options=options
+    )
+
+    driver.set_window_size(3200, 5120)
+    driver.get(url)
+
+    yield driver
+    driver.close()
